1602bf31c4da035f67d966dfe35f2f2edcd68e4b
a-slide <adrien.leger@gmail.com>
2015-02-05 18:53:02 +0100

    Refactoring code and partially fixing memory leak

--- src/Quade.py
+++ src/Quade.py
@@ -25,7 +25,6 @@ try:
     import numpy
     import HTSeq
 
-
 except ImportError as E:
     print (E)
     print ("Please verify your dependencies. See Readme for more informations\n")
@@ -62,7 +61,6 @@ class Quade(object):
             cp.read(self.conf)
 
             # Quality section
-            self.mean_qual = cp.getint("quality", "mean_qual")
             self.minimal_qual = cp.getint("quality", "minimal_qual")
             self.qual_scale = cp.get("quality", "qual_scale")
 
@@ -73,24 +71,24 @@ class Quade(object):
             self.mol2 = self.idx2 and cp.getboolean("index", "molecular2")
 
             # Positions of subindex
-            self.idx1_pos = [
-                cp.getint("index", "index1_start")-1,
-                cp.getint("index", "index1_end")] if self.idx1 else [0,0]
-            self.idx2_pos = [
-                cp.getint("index", "index2_start")-1,
-                cp.getint("index", "index2_end")] if self.idx2 else [0,0]
-            self.mol1_pos = [
-                cp.getint("index", "molecular1_start")-1,
-                cp.getint("index", "molecular1_end")] if self.mol1 else [0,0]
-            self.mol2_pos = [
-                cp.getint("index", "molecular2_start")-1,
-                cp.getint("index", "molecular2_end")] if self.mol2 else [0,0]
+            self.idx1_pos = {
+                "start":cp.getint("index", "index1_start")-1,
+                "end":cp.getint("index", "index1_end")}
+            self.idx2_pos = {
+                "start":0 if not self.idx2 else cp.getint("index", "index2_start")-1,
+                "end":0 if not self.idx2 else cp.getint("index", "index2_end")}
+            self.mol1_pos = {
+                "start":0 if not self.mol1 else cp.getint("index", "molecular1_start")-1,
+                "end":0 if not self.mol1 else cp.getint("index", "molecular1_end")}
+            self.mol2_pos = {
+                "start":0 if not self.mol2 else cp.getint("index", "molecular2_start")-1,
+                "end":0 if not self.mol2 else cp.getint("index", "molecular2_end")}
 
             # List of fastq files
             self.seq_R1 = cp.get("fastq", "seq_R1").split()
             self.seq_R2 = cp.get("fastq", "seq_R2").split()
-            self.index_R1 = cp.get("fastq", "index_R1").split() if self.idx1 else []
-            self.index_R2 = cp.get("fastq", "index_R2").split() if self.idx2 else []
+            self.index_R1 = cp.get("fastq", "index_R1").split()
+            self.index_R2 = [] if not self.idx2 else cp.get("fastq", "index_R2").split()
 
             # Output option sections
             self.write_undetermined = cp.getboolean("output", "write_undetermined")
@@ -108,13 +106,20 @@ class Quade(object):
                     index_seq = (cp.get(sample, "index1_seq")).upper()
 
                 # Create a autoreferenced Sample object
-                Sample(name=cp.get(sample, "name"), index=index_seq, write_pass=True , write_fail=self.write_fail)
-
+                Sample(
+                    name=cp.get(sample, "name"),
+                    index=index_seq,
+                    write_pass=True ,
+                    write_fail=self.write_fail,
+                    minimal_qual=self.minimal_qual)
+                    
             # Create a Sample object for undetermined reads
-            if self.write_undetermined :
-                Sample (name="Undetermined", index="NNNNNN", write_pass=True , write_fail=self.write_fail)
-            else:
-                Sample (name="Undetermined", index="NNNNNN", write_pass=False , write_fail=False)
+            Sample (
+                name="Undetermined",
+                index="N",
+                write_pass=True if self.write_undetermined else False,
+                write_fail=self.write_fail if self.write_undetermined else False,
+                minimal_qual=0)
 
             # Values are tested in a private function
             self._test_values()
@@ -139,6 +144,7 @@ class Quade(object):
     def __str__(self):
         return "<Instance of {} from {} >\n".format(self.__class__.__name__, self.__module__)
 
+
     #~~~~~~~PUBLIC METHODS~~~~~~~#
 
     def __call__(self):
@@ -153,11 +159,11 @@ class Quade(object):
             self.simple_index_parser()
 
         # Flush remaining content in sample buffers
-        Sample.flush_all()
+        Sample.FLUSH_ALL()
 
         if self.write_report:
             print ("Generate_report")
-            Sample.write_report()
+            Sample.WRITE_REPORT()
 
         print ("Done")
         return(0)
@@ -165,49 +171,73 @@ class Quade(object):
     def double_index_parser (self):
 
         # Iterate over fastq chunks for sequence and index reads
-        for n, (S1, S2, I1, I2) in enumerate (zip (self.seq_R1, self.seq_R2, self.index_R1, self.index_R2)):
+        for n, (R1, R2, I1, I2) in enumerate (zip (self.seq_R1, self.seq_R2, self.index_R1, self.index_R2)):
 
             print("Parsing chunk {}/{}".format(n+1, len(self.seq_R1)))
 
             # Init HTSeq fastq reader generator
-            S1_gen = HTSeq.FastqReader(S1, qual_scale=self.qual_scale)
-            S2_gen = HTSeq.FastqReader(S2, qual_scale=self.qual_scale)
+            R1_gen = HTSeq.FastqReader(R1, qual_scale=self.qual_scale)
+            R2_gen = HTSeq.FastqReader(R2, qual_scale=self.qual_scale)
             I1_gen = HTSeq.FastqReader(I1, qual_scale=self.qual_scale)
             I2_gen = HTSeq.FastqReader(I2, qual_scale=self.qual_scale)
 
             # Iterate over read in fastq files and put each set in a list
-            for s1, s2, i1, i2 in zip (S1_gen, S2_gen, I1_gen, I2_gen):
+            for read1, read2, index1, index2 in zip (R1_gen, R2_gen, I1_gen, I2_gen):
 
-                # Extract index and molecular sequences from index reads and merge them
-                index = (i1.seq[self.idx1_pos[0]:self.idx1_pos[1]]+i2.seq[self.idx2_pos[0]:self.idx2_pos[1]]).upper()
-                molecular = (i1.seq[self.mol1_pos[0]:self.mol1_pos[1]]+i2.seq[self.mol2_pos[0]:self.mol2_pos[1]]).upper()
-
+                # Extract index and molecular sequences from index reads and merge
+                index = HTSeq.SequenceWithQualities(
+                    seq = index1.seq[self.idx1_pos["start"]:self.idx1_pos["end"]] + index2.seq[self.idx2_pos["start"]:self.idx2_pos["end"]],
+                    name = "fused_index",
+                    qualstr = index1.qualstr[self.idx1_pos["start"]:self.idx1_pos["end"]] + index2.qualstr[self.idx2_pos["start"]:self.idx2_pos["end"]],
+                    qualscale = self.qual_scale)
+                
+                molecular = HTSeq.SequenceWithQualities(
+                    seq = index1.seq[self.mol1_pos["start"]:self.mol1_pos["end"]] + index2.seq[self.mol2_pos["start"]:self.mol2_pos["end"]],
+                    name = "fused_molecular",
+                    qualstr = index1.qualstr[self.mol1_pos["start"]:self.mol1_pos["end"]] + index2.qualstr[self.mol2_pos["start"]:self.mol2_pos["end"]],
+                    qualscale = self.qual_scale)
+                
                 # Identify sample and verify index quality
-                pass_quality = self._quality_filter(i1.qual) and self._quality_filter(i2.qual)
-                Sample.finder (s1, s2, index, molecular, pass_quality)
+                Sample.FINDER (read1,read2, index, molecular)
+                
+            # Update report
+            if self.write_report:
+                Sample.WRITE_REPORT()
 
     def simple_index_parser (self):
 
         # Iterate over fastq chunks for sequence and index reads
-        for n, (S1, S2, I1) in enumerate (zip(self.seq_R1, self.seq_R2, self.index_R1)):
+        for n, (R1, R2, I1) in enumerate (zip(self.seq_R1, self.seq_R2, self.index_R1)):
 
             print("Parsing chunk {}/{}".format(n+1, len(self.seq_R1)))
 
             # Init HTSeq fastq reader generator
-            S1_gen = HTSeq.FastqReader(S1, qual_scale=self.qual_scale)
-            S2_gen = HTSeq.FastqReader(S2, qual_scale=self.qual_scale)
+            R1_gen = HTSeq.FastqReader(R1, qual_scale=self.qual_scale)
+            R2_gen = HTSeq.FastqReader(R2, qual_scale=self.qual_scale)
             I1_gen = HTSeq.FastqReader(I1, qual_scale=self.qual_scale)
 
             # Iterate over read in fastq files and put each set in a list
-            for s1, s2 ,i1 in zip (S1_gen, S2_gen, I1_gen):
-
-                # Extract index and molecular sequences from index read
-                index = (i1.seq[self.idx1_pos[0]:self.idx1_pos[1]]).upper()
-                molecular = (i1.seq[self.mol1_pos[0]:self.mol1_pos[1]]).upper()
-
+            for read1, read2 ,index1 in zip (R1_gen, R2_gen, I1_gen):
+                
+                # Extract index and molecular sequences from index reads 
+                index = HTSeq.SequenceWithQualities(
+                    seq = index1.seq[self.idx1_pos["start"]:self.idx1_pos["end"]],
+                    name = "index",
+                    qualstr = index1.qualstr[self.idx1_pos["start"]:self.idx1_pos["end"]],
+                    qualscale = self.qual_scale)
+                
+                molecular = HTSeq.SequenceWithQualities(
+                    seq = index1.seq[self.mol1_pos["start"]:self.mol1_pos["end"]],
+                    name = "molecular",
+                    qualstr = index1.qualstr[self.mol1_pos["start"]:self.mol1_pos["end"]],
+                    qualscale = self.qual_scale)
+                
                 # Identify sample and verify index quality
-                pass_quality = self._quality_filter(i1.qual)
-                Sample.finder (s1, s2, index, molecular, pass_quality)
+                Sample.FINDER (read1,read2, index, molecular)
+                
+            # Update report
+            if self.write_report:
+                Sample.WRITE_REPORT()
 
 
     #~~~~~~~PRIVATE METHODS~~~~~~~#
@@ -236,7 +266,6 @@ class Quade(object):
         """
         # Verify values from the quality section
         assert self.qual_scale in ["solexa-old", "solexa-old", 'phred'], "Authorized values for quality_scale : solexa-old, solexa-old, phred"
-        assert 0 <= self.mean_qual <= 40, "Authorized values for mean_qual : 0 to 40"
         assert 0 <= self.minimal_qual <= 40, "Authorized values for minimal_qual : 0 to 40"
 
         # Verify values of the fastq section and readability of fastq files
@@ -250,15 +279,10 @@ class Quade(object):
             self._is_readable_file (self.seq_R1 + self.seq_R2 + self.index_R1)
 
         # Verify values of the index section
-        for start, end in [self.idx1_pos, self.idx2_pos, self.mol1_pos, self.mol2_pos]:
-            assert start >= 0
-            assert end >= start
+        for pos in [self.idx1_pos, self.idx2_pos, self.mol1_pos, self.mol2_pos]:
+            assert pos["start"] >= 0
+            assert pos["end"] >= pos["start"]
 
-    def _quality_filter(self, qual):
-        """
-        Test minimal quality and mean quality of a phred quality list qual
-        """
-        return min(qual) >= self.minimal_qual and sum(qual)/len(qual) >= self.mean_qual
 
     def _is_readable_file (self, file_list):
         """
@@ -267,6 +291,7 @@ class Quade(object):
         for fp in file_list:
             if not os.access(fp, os.R_OK):
                 raise ValueError ("{} is not a valid file".format(fp))
+                
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 class Sample(object):
@@ -295,13 +320,18 @@ class Sample(object):
         self.TOTAL_PASS += 1
 
     @ classmethod
-    def finder (self, read1, read2, index="", molecular="", pass_quality=True):
-        # Try to find the sample corresponding to the index, by default use the Undetermined sample
-        sample = self.INDEX_TO_SAMPLE.get(index, self.NAME_TO_SAMPLE["Undetermined"])
-        sample(read1, read2, index, molecular, pass_quality)
+    def FINDER (self, read1, read2, index, molecular=""):
+        """
+        Try to find the sample corresponding to the index sequence, Undetermined by default
+        """
+        # Look up in the sample dict access by INDEX sequence
+        sample = self.INDEX_TO_SAMPLE.get(index.seq, self.NAME_TO_SAMPLE["Undetermined"])
+        
+        # Call the Sample object by the __call__ method
+        sample(read1, read2, index, molecular)
 
     @ classmethod
-    def flush_all (self):
+    def FLUSH_ALL (self):
         for sample in self.NAME_TO_SAMPLE.values():
             if sample.write_pass:
                 sample._flush_to_file(file = sample.R1_pass_file, buffer = sample.R1_pass_buffer)
@@ -311,37 +341,44 @@ class Sample(object):
                 sample._flush_to_file(file = sample.R2_fail_file, buffer = sample.R2_fail_buffer)
 
     @ classmethod
-    def write_report (self):
+    def WRITE_REPORT (self):
         with open ("Quade_report.txt", "wb") as report:
             report.write ("Total pair analysed\t{}\n".format(self.TOTAL))
             report.write ("Total pair pass quality\t{} ({}%)\n".format(self.TOTAL_PASS, self.TOTAL_PASS*100/self.TOTAL))
             report.write ("Total pair fail quality\t{} ({}%)\n".format(self.TOTAL_FAIL, self.TOTAL_FAIL*100/self.TOTAL))
-            report.write ("Number of samples\t{}\n".format(len(self.NAME_TO_SAMPLE.values())))
-            sample_list = [sample for sample in self.NAME_TO_SAMPLE.keys()]
+            report.write ("Number of samples\t{}\n".format(len(self.NAME_TO_SAMPLE.values())-1))
+            sample_list = [sample for sample in self.NAME_TO_SAMPLE.keys() if sample != "Undetermined"]
             sample_list.sort()
             for sample in sample_list:
                 report.write ("\nSample Name\t{}\n".format(self.NAME_TO_SAMPLE[sample].name))
                 report.write ("\tIndex sequence\t{}\n".format(self.NAME_TO_SAMPLE[sample].index))
-                report.write ("\tTotal pair analysed\t{} ({}% all)\n".format(
-                    self.NAME_TO_SAMPLE[sample].total,
-                    self.NAME_TO_SAMPLE[sample].total * 100 / self.TOTAL))
-
-                report.write ("\tTotal pair pass quality\t{} ({}% sample)\n".format(
-                    self.NAME_TO_SAMPLE[sample].total_pass,
-                    self.NAME_TO_SAMPLE[sample].total_pass * 100 / self.NAME_TO_SAMPLE[sample].total))
-                report.write ("\tTotal pair fail quality\t{} ({}% sample)\n".format(
-                    self.NAME_TO_SAMPLE[sample].total_fail,
-                    self.NAME_TO_SAMPLE[sample].total_fail * 100 / self.NAME_TO_SAMPLE[sample].total))
+                if self.NAME_TO_SAMPLE[sample].total == 0:
+                    report.write ("\tNo read found\n") 
+                else:
+                    report.write ("\tTotal pair analysed\t{} ({}% all)\n".format(
+                        self.NAME_TO_SAMPLE[sample].total,
+                        self.NAME_TO_SAMPLE[sample].total * 100 / self.TOTAL))
+                    report.write ("\tTotal pair pass quality\t{} ({}% sample)\n".format(
+                        self.NAME_TO_SAMPLE[sample].total_pass,
+                        self.NAME_TO_SAMPLE[sample].total_pass * 100 / self.NAME_TO_SAMPLE[sample].total))
+                    report.write ("\tTotal pair fail quality\t{} ({}% sample)\n".format(
+                        self.NAME_TO_SAMPLE[sample].total_fail,
+                        self.NAME_TO_SAMPLE[sample].total_fail * 100 / self.NAME_TO_SAMPLE[sample].total))
+            report.write ("\nUndetermined reads\n")
+            report.write ("\tTotal pair analysed\t{} ({}% all)\n".format(
+                self.NAME_TO_SAMPLE["Undetermined"].total,
+                self.NAME_TO_SAMPLE["Undetermined"].total * 100 / self.TOTAL))
 
     #~~~~~~~FUNDAMENTAL METHODS~~~~~~~#
 
-    def __init__ (self, name, index, write_pass = True, write_fail = True):
+    def __init__ (self, name, index, write_pass=True, write_fail=False, minimal_qual=0):
 
         # Create self variables
         self.name = name
-        self.index = index
+        self.index = index 
         self.write_pass = write_pass
-        self.write_fail = write_fail
+        self.write_fail = write_fail if minimal_qual > 0 else False # write fail if quality filtering only
+        self.minimal_qual = minimal_qual
 
         # test uniqueness of name and index and DNA composition of index
         assert self.name not in self.NAME_TO_SAMPLE, "{} : Name is not unique".format(self.name)
@@ -389,15 +426,14 @@ class Sample(object):
 
     #~~~~~~~PUBLIC METHODS~~~~~~~#
 
-    def __call__(self, read1, read2, index="", molecular="", pass_quality=True):
+    def __call__(self, read1, read2, index, molecular=""):
 
-        #print ("Read attributed to "+ self.name)
         # Increment class and object total read counter
         self.increment_TOTAL()
         self.total += 1
 
         # If the index quality is good
-        if pass_quality:
+        if self._pass_quality(index):
 
             # Increment counters
             self.increment_TOTAL_PASS()
@@ -409,7 +445,7 @@ class Sample(object):
                 self.R1_pass_buffer += self._fastq_str (read1, index, molecular)
                 self.R2_pass_buffer += self._fastq_str (read2, index, molecular)
 
-                if self.current_pass == self.BUFFER_SIZE:
+                if self.current_pass >= self.BUFFER_SIZE:
                     # Empty buffer in file and reset counter and buffers
                     self._flush_to_file (file = self.R1_pass_file, buffer = self.R1_pass_buffer)
                     self._flush_to_file (file = self.R2_pass_file, buffer = self.R2_pass_buffer)
@@ -430,39 +466,53 @@ class Sample(object):
                 self.R1_fail_buffer += self._fastq_str (read1, index, molecular)
                 self.R2_fail_buffer += self._fastq_str (read2, index, molecular)
 
-                if self.current_fail == self.BUFFER_SIZE:
+                if self.current_fail >= self.BUFFER_SIZE:
 
                     # Empty buffer in file and reset counter and buffers
                     self._flush_to_file (file = self.R1_fail_file, buffer = self.R1_fail_buffer)
                     self._flush_to_file (file = self.R2_fail_file, buffer = self.R2_fail_buffer)
                     self.current_fail = 0
                     self.R1_fail_buffer = ""
-                self.R2_fail_buffer = ""
+                    self.R2_fail_buffer = ""
 
 
     #~~~~~~~PRIVATE METHODS~~~~~~~#
 
     def _is_dna (self, sequence):
+        """
+        Verify if the sequence is DNA
+        """
         for base in sequence:
             if base not in self.DNA:
                 return False
         return True
 
+    def _pass_quality(self, seq):
+        """
+        Test minimal quality of a phred quality Seq
+        """
+        return min(seq.qual) >= self.minimal_qual
+
     def _fastq_str (self, read, index, molecular=""):
-
+        """
+        Generate a fastq str of read from read, index and molecular Seq Record
+        """
         if molecular:
-            read_name = "{}:{}:{}:".format(read.name, index, molecular)
+            return "@{}:{}:{}\n{}\n+\n{}\n".format(read.name.split()[0], index.seq, molecular.seq, read.seq, read.qualstr)
         else:
-            read_name = "{}:{}:".format(read.name, index)
-
-        return "@{}\n{}\n+\n{}\n".format(read_name, read.seq, read.qualstr)
+            return "@{}:{}\n{}\n+\n{}\n".format(read.name.split()[0], index.seq, read.seq, read.qualstr)
 
     def _init_file (self, file):
+        """
+        Initialize an empty file = erase existing file
+        """
         with gzip.open (file, "wb") as fastq_file:
             pass
 
     def _flush_to_file (self, file, buffer):
-        #print("Flush buffer in file "+file)
+        """
+        Flush str buffer in file 
+        """
         with gzip.open (file, "ab") as fastq_file:
             fastq_file.write(buffer)
 
